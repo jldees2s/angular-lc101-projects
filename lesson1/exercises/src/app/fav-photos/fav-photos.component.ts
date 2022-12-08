8bb021e84895da559ccfabbcb9c15edd0afd92a3
“jldes2s” <jordanldees@gmail.com>
2022-12-07 19:15:26 -0600

    part 1

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
-  image1 = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
-  image2 = '';
-  image3 = '';
+  photosTitle = 'My Favorite Photos';
+  image1 = 'https://blog.spcollege.edu/careers-internships/wp-content/uploads/sites/22/2019/05/launchcode-logo-facebook1-e397a364ce733d0dee2bca438c6c1cf45d4ba9200a28c7b0b0da0e436f0ae44f.jpg';
+  image2 = 'https://media.npr.org/assets/img/2021/08/11/gettyimages-1279899488_wide-f3860ceb0ef19643c335cb34df3fa1de166e2761-s1100-c50.jpg';
+  image3 = 'https://ychef.files.bbci.co.uk/976x549/p07ryyyj.jpg';
 
   constructor() { }
 
